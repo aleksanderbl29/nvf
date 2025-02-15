7e425ba483d4bdb43896d305d326c599fcfcb902
aleksanderbl29 <github@aleksanderbl.dk>
2025-02-15 09:14:28 +0100

    Format things with alejandra

--- flake.nix
+++ flake.nix
@@ -7,43 +7,46 @@
     };
   };
 
-  outputs = { self, nixpkgs, nvf }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in {
-      packages = forAllSystems (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        neovimConfigured = nvf.lib.neovimConfiguration {
-          inherit pkgs;
-          modules = [
-            {
-              config.vim = {
-                viAlias = true;
-                vimAlias = true;
+  outputs = {
+    self,
+    nixpkgs,
+    nvf,
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      neovimConfigured = nvf.lib.neovimConfiguration {
+        inherit pkgs;
+        modules = [
+          {
+            config.vim = {
+              viAlias = true;
+              vimAlias = true;
 
-                theme = {
-                  enable = true;
-                  name = "catppuccin";
-                  style = "mocha";
-                  transparent = true;
-                };
-
-                languages.nix.enable = true;
-                dashboard.startify.enable = true;
-                treesitter.enable = true;
-                filetree.neo-tree.enable = true;
+              theme = {
+                enable = true;
+                name = "catppuccin";
+                style = "mocha";
+                transparent = true;
               };
-            }
-          ];
-        };
-      in {
-        default = neovimConfigured.neovim;
-      });
-    };
+
+              languages.nix.enable = true;
+              dashboard.startify.enable = true;
+              treesitter.enable = true;
+              filetree.neo-tree.enable = true;
+            };
+          }
+        ];
+      };
+    in {
+      default = neovimConfigured.neovim;
+    });
+  };
 }
